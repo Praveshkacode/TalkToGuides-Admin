24d86da1e89fc00499d30404f5281c9a2de87ee6
Praveshkacode <pravesh182003@gmail.com>
2025-05-11 01:30:29 +0530

    add expertlist page

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,14 +1,45 @@
 import { createContext, useState } from "react";
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
     const [aToken,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
+    const [experts,setExperts] = useState([])
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
+    const getAllExperts = async () => {
+        try {
+            const {data} = await axios.post(backendUrl+'/api/admin/all-experts',{},{headers:{aToken}})
+            if(data.success){
+                setExperts(data.experts)
+                console.log(data.experts);
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const changeAvailability = async (expId) => {
+        try {
+            const {data} = await axios.post(backendUrl+'/api/admin/change-availability',{expId},{headers:{aToken}})
+            if(data.success){
+                toast.success(data.message)
+                getAllExperts()
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
-        aToken,setAToken,backendUrl,
+        aToken,setAToken,backendUrl,experts,getAllExperts,changeAvailability
     }
     return (
         <AdminContext.Provider value={value}>
